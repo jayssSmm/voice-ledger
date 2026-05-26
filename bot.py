371d7fd16e13ea25b1714c7a1a4944f7cb8172e8
Samman jaiswal <214768065+jayssSmm@users.noreply.github.com>
2026-05-26 11:24:16 +0000

    the ping server only handles GET but UptimeRobot is sending a HEAD request by default. One-line fix

--- bot.py
+++ bot.py
@@ -200,6 +200,10 @@ class _PingHandler(BaseHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(b"OK")
  
+    def do_HEAD(self):
+        self.send_response(200)
+        self.end_headers()
+ 
     def log_message(self, *args):
         pass  # silence access logs
  
